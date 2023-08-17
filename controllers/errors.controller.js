ce3bdeef2433ea9f9858d082463e8a070164b096
Asme Mussie <48681092+asme10@users.noreply.github.com>
2023-08-17 21:19:42 +0100

    Update  errors.controller.js

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,16 +1,21 @@
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
-  } else next(err);
+  } else {
+    next(err);
+  }
 };
-
 exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
-  } else next(err);
+  } else if (err.code === "23503") {
+    res.status(404).send({ msg: "Not found" });
+  } else if (err.code === "23502") {
+    res.status(400).send({ msg: "Bad request: Missing required field(s)" });
+  } else {
+    next(err);
+  }
 };
-
 exports.handleServerErrors = (err, req, res, next) => {
-  console.log(err);
   res.status(500).send({ msg: "Internal Server Error" });
 };
