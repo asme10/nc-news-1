6e15d3489cc23e3e4253a4462de140e5b205bb81
Asme Mussie <asmebold@gmail.com>
2023-08-17 08:55:31 +0100

    get all comments for an article

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const {
 
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticleById, getAllArticles } = require("./controllers/articles.controller");
+const {
+  getArticleById,
+  getAllArticles,
+} = require("./controllers/articles.controller");
+const { getCommentsForArticle } = require("./controllers/comments.controller");
 
 app.get("/api", getApi);
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
 app.get("/api/articles", getAllArticles);
+app.get("/api/articles/:article_id/comments", getCommentsForArticle);
 
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
